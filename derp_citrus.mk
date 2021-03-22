91f19f4b00f0738c4ad86c073aba4885f2c2a508
hijemu <joboyjhem24@gmail.com>
2021-03-22 21:34:14 +0800

    citrus: Get Derped! Official-ly

--- derp_citrus.mk
+++ derp_citrus.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
+#build Official-ly
+DERP_BUILDTYPE := Official
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := derp_citrus
 PRODUCT_DEVICE := citrus
